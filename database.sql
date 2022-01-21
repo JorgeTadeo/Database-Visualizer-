bf9bde497790873308ca2a8a03ab6d829a7ce221
JorgeTadeo <jorge_tadeo@icloud.com>
2022-01-20 23:29:46 -0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -7,10 +7,10 @@ CREATE TABLE Customer (
 
 CREATE TABLE ItemOrder (
 	DatePlaced date,
-	confirmation# int,
+	confirmation int,
 	ItemID int,
 	CustomerID int,
-	primary key(confirmation#,ItemID,CustomerID),
+	primary key(confirmation,ItemID,CustomerID),
 	foreign key(CustomerID) references Customer(CustomerID),
 	foreign key(ItemID) references Item(ItemID)
 );
@@ -18,7 +18,7 @@ CREATE TABLE ItemOrder (
 CREATE TABLE Item (
 	Name varchar(30),
 	Price decimal(4,2),
-	ItemID int
+	ItemID int,
 	primary key(ItemID)
 );
 
@@ -27,7 +27,14 @@ VALUES ('Shirt', 9.99, 12345678),
 	   ('Pants', 24.99, 12345677),
 	   ('shoes', 19.99, 12345666),
 	   ('Sweater', 14.99, 12345555);
+	   
 INSERT INTO Customer
 VALUES (11111111, 'JorgeTadeo510@gmail.com', 'Mermaid123'),
 	   (00000000, 'Guest@mail.com', 'Guest123');
+	   
 INSERT INTO ItemOrder
+VALUES ('2020-09-08', 9877827, 12345666, 0),
+       ('2020-09-08', 9877827, 12345677, 0),
+       ('2020-09-08', 9877827, 12345678, 0),
+       ('2020-09-08', 9989988, 12345555, 11111111);
+       
